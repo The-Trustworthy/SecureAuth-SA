58942ec1f6dc7597d64eb9d76b9672311a57cae0
Ravi Prakash <1907prakash.ravi@gmail.com>
2022-05-22 13:30:18 +0530

    final salting updates

--- src/modules/preSalting.py
+++ src/modules/preSalting.py
@@ -2,6 +2,7 @@
 import binascii
 import base64
 import hashlib
+from os import strerror
 import numpy as np
 from PIL import Image,ImageOps
 
@@ -10,11 +11,17 @@ class preSalting:
   def __init__(self):
     return
   
-  def getNumericVal(self, gotStr: str):
+  def getNumericStr(self, gotStr: str):
     strBytes = gotStr.encode()
     hexStr = binascii.hexlify(strBytes)
-    strNum = int(hexStr.decode('utf8'))
+    strNum = hexStr.decode('utf8')
     return strNum
+  
+  def getSHA256(self, data: str):
+    encData=data.encode()
+    dataHash = hashlib.sha256(encData)
+    dataHashStr = dataHash.hexdigest()
+    return dataHashStr
   
   def mapToSingleDigit(self, num):
     if len(str(num)) != 1:
@@ -27,28 +34,50 @@ class preSalting:
     else:
       return num
   
-  def processBiometric(self, fName):
-    im = Image.open(fName)
-    im = ImageOps.grayscale(im)
-    imarr = np.array(im)
-    imarr = imarr.reshape(imarr.shape[0]*imarr.shape[1],)
-    bioFPStr = str(sum(imarr))
-    # fp hash (sha 256)
-    encodedFP=bioFPStr.encode()
-    fpHash = hashlib.sha256(encodedFP)
-    fpHashStr = fpHash.hexdigest()
-    res = self.__getNumericVal(fpHashStr)
-    return res
-
-  def processDOB(self, dob):
-    dob = str(dob)
-    dob = dob.split('/')
-    dob = int(''.join(dob))
-    res = self.__mapToSingleDigit(dob)
-    return res
+  ##########################
   
-  ########
+  def processBiometric(self, fileName: str):
+    try:
+      im = Image.open(fileName)
+      im = ImageOps.grayscale(im)
+      imarr = np.array(im)
+      imarr = imarr.reshape(imarr.shape[0]*imarr.shape[1],)
+      bioFPStr = str(sum(imarr))
+      # fp hash (sha 256)
+      fpHashStr = self.__getSHA256(bioFPStr)
+    except:
+      print("Invalid Fingerprint!!")
+      fpHashStr = False
+    finally:
+      return fpHashStr
 
+  def processDOB(self, dob: str):
+    try:
+      dob = str(dob)
+      dob = dob.split('/')
+      dobInt = int(''.join(dob))
+      dobUniqueNum = self.__mapToSingleDigit(dobInt)
+      dobProduct = str(dobInt * dobUniqueNum)
+      dobProdHash = self.__getSHA256(dobProduct)
+    except:
+      print("Invalid DoB!!")
+      dobProdHash = False
+    finally:
+      return dobProdHash
+  
+  def processPass(self, password: str):
+    try:
+      passNumStr = self.__getNumericStr(password)
+      passNumHash = self.__getSHA256(passNumStr)
+    except:
+      print("Invalid DoB!!")
+      passNumHash = False
+    finally:
+      return passNumHash
+
+  ##########################
+  
   __mapToSingleDigit = mapToSingleDigit
-  __getNumericVal = getNumericVal
+  __getNumericStr = getNumericStr
+  __getSHA256 = getSHA256
   
